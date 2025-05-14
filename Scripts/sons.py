583b14b1747ae3fb572656d77d1d59f36decd4b9
Quentin RICHARD <149069496+QuentinRICHARD17@users.noreply.github.com>
2025-05-14 16:50:04 +0200

    Update sons.py

--- Scripts/sons.py
+++ Scripts/sons.py
@@ -2,6 +2,7 @@ import pygame
 pygame.mixer.init()
 
 son_explosion = pygame.mixer.Sound("ressources/Sounds/Explosion_Myinstants.mp3")
+son_explosion.set_volume(0.3)  # 0.0 = muet et 1.0 = max
 son_game_over = pygame.mixer.Sound("ressources/Sounds/GameOver_Myinstants.mp3")
 musique_fond = "ressources/Sounds/MusiqueDeFondPiratesLibreDeDroit.mp3"
 son_compte_rebours = pygame.mixer.Sound("ressources/Sounds/3_2_1_Go_Myinstants.mp3")
@@ -17,4 +18,3 @@ def jouer_musique_de_fond():
 
 def jouer_compte_rebours():
     son_compte_rebours.play()
-#met les sons ici lucass
