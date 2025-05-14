785b4d7e69e226f954d0f289cd2149f3f3399556
lucas <lucas.daugan@efrei.net>
2025-05-14 15:59:51 +0200

    sons ajouté by quentin prime

--- Scripts/sons.py
+++ Scripts/sons.py
@@ -1,1 +1,20 @@
+import pygame
+pygame.mixer.init()
+
+son_explosion = pygame.mixer.Sound("ressources/Sounds/Explosion_Myinstants.mp3")
+son_game_over = pygame.mixer.Sound("ressources/Sounds/GameOver_Myinstants.mp3")
+musique_fond = "ressources/Sounds/MusiqueDeFondPiratesLibreDeDroit.mp3"
+son_compte_rebours = pygame.mixer.Sound("ressources/Sounds/3_2_1_Go_Myinstants.mp3")
+
+def jouer_son_explosion():
+    son_explosion.play()
+def jouer_son_game_over():
+    son_game_over.play()
+
+def jouer_musique_de_fond():
+    pygame.mixer.music.load(musique_fond)
+    pygame.mixer.music.play(-1)  # "-1" fait tourner en boucle
+
+def jouer_compte_rebours():
+    son_compte_rebours.play()
 #met les sons ici lucas
